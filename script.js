972532570cfecbdeff2c549b8315aa760e784855
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-05-24 18:33:55 -0400

    Completed styling

---
+++ script.js
@@ -0,0 +1,5 @@
+const wrapper = document.querySelector(".wrapper"),
+inputPart = wrapper.querySelector(".input-parrt"),
+infoTxt = inputPart.querySelector("info-txt"),
+inputField = inputPart.querySelector("input");
+
